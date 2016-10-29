23d0a8c0de1f188a3d4f121b4bb1a0d45657afc0
mainyaa <mainya@gmail.com>
2016-10-29 22:47:04 +0900

    fix: code

--- im2txt/inference_utils/vocabulary.py
+++ im2txt/inference_utils/vocabulary.py
@@ -46,7 +46,7 @@ class Vocabulary(object):
 
     with tf.gfile.GFile(vocab_file, mode="r") as f:
       reverse_vocab = list(f.readlines())
-    reverse_vocab = [line.split()[0] for line in reverse_vocab]
+    reverse_vocab = [eval(line.split()[0]) for line in reverse_vocab]
     assert start_word in reverse_vocab
     assert end_word in reverse_vocab
     if unk_word not in reverse_vocab:
